713f9d2f5272a3fa4de168e0b487533e8f7a333c
Dermot Fitzpatrick <39203833+tomredf@users.noreply.github.com>
2021-10-25 00:54:21 -0700

    Removed comments

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
 params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
@@ -24,7 +21,6 @@ params = {
 res = requests.get(STOCK_ENDPOINT, params=params)
 data = res.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
-# print(data_list)
 current = float(data_list[0]["4. close"])
 previous = float(data_list[1]["4. close"])
 diff = abs(current - previous)
@@ -36,8 +32,6 @@ if current > previous:
 else:
     direction = "🔻"
 
-#print(current, previous, diff, direction, percent)
-
 if percent > 1.5:
     news_params = {
         "qInTitle": COMPANY_NAME,
@@ -47,15 +41,13 @@ if percent > 1.5:
     articles = res.json()["articles"]
     top_3 = articles[:3]
 
-    news_list = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in top_3]
+    news_list = [f"{STOCK_NAME} {direction} {round(percent, 1)}% \nHeadline: {article['title']}. \nBrief: {article['description']}" for article in top_3]
 
     client = Client(twilio_account_sid, twilio_auth_token)
 
-    msg_start = f"{STOCK_NAME} {direction} {round(percent, 1)}% \n"
-
     for msg in news_list:
         message = client.messages.create(
-            body=msg_start + msg,
+            body=msg,
             from_=from_phone,
             to=to_phone,
         )
